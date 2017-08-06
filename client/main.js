5b0f4bac1cdedc361da77169766a8b5eb20c424a
Laurence <laurence.j.squires@gmail.com>
2017-08-06 14:33:16 +0100

    added client side pruning

--- client/main.js
+++ client/main.js
@@ -5,24 +5,21 @@ txs = new Mongo.Collection('txs');
 var cola = require("webcola");
 var d3 = require('d3-3');
 var minsAgo = 0.5;
+var nextClean = new Date();
 
-SyncedCron.add({
-  name: 'Clean client side of bad entries',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.text('every 10 seconds');
-  },
-  job: function() {
-    console.log("doing job cs");
-    var now = new Date((new Date()).getTime() - minsAgo*60000);
-    txs.remove({time: { $lt: now}});
+  function cleanTXS() {
+    if(new Date() > nextClean) {
+      nextClean = new Date((new Date()).getTime() + 10000)
+      console.log("doing job cs");
+      var now = new Date((new Date()).getTime() - minsAgo*60000);
+      txs.remove({time: { $lt: now}});
+    }
     /*txs.find().forEach(function (item) {
       if(item.time < now) {
         txs.remove({_id: item._id});
       }
     });*/
   }
-});
 
 Router.route('/', {name:"Home"},function () {
   this.render('Home');
@@ -129,6 +126,7 @@ Template.vis.rendered = function () {
     Meteor.subscribe("txs", minsAgo);
     const handle = txs.find().observeChanges({
       added: function (id, fields) {
+        cleanTXS();
         var node = {x: centerx, y: centery, tx: fields, id: id, colour: getColour(fields)};
         nodes.push(node);
         nodes.forEach(function (target) {
