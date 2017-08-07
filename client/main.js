acec664794e6a826b22b7f1d275cc94e7b3b47bb
Laurence <laurence.j.squires@gmail.com>
2017-08-07 19:26:06 +0100

    debugging added highlighting of nodes on hover

--- client/main.js
+++ client/main.js
@@ -227,7 +227,7 @@ Template.vis.rendered = function () {
             if(selected && !d3.select("#a"+selected).empty()) {
               d3.select("#a"+selected).transition().duration(200).attr("r", nodeRadius);
             }
-            console.log(d.id+" = "+this);
+            console.log(d.id+" = "+this.id);
             d3.select(this).transition().duration(200).attr("r", nodeRadius*2);
             selected = d.id;
             hover.html(JSON.stringify(d.tx));
