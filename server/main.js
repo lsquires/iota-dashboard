3c7351d986e8b4a8a5de3c4af6988021c5c80220
Laurence <laurence.j.squires@gmail.com>
2017-08-10 16:41:15 +0100

    restructuring database

--- server/main.js
+++ server/main.js
@@ -30,7 +30,7 @@ Meteor.publish('txs', function (minsAgo, filterConfirmed) {
   });
 });
 
-Meteor.startup(function() {
+Meteor.startup(() => {
   console.log("server");
 
 
@@ -75,7 +75,7 @@ Meteor.startup(function() {
     ignored: /[\/\\]\./, persistent: true
   });
 
-  /*watcher.on('add', Meteor.bindEnvironment(function(path) {
+  watcher.on('add', Meteor.bindEnvironment(function(path) {
     console.log(path);
     newFile=fs.readFileSync(path,'utf8');
     let split = newFile.split(/\r?\n/);
@@ -83,7 +83,7 @@ Meteor.startup(function() {
 
 	  addTX(tx, path);
 
-  }));*/
+  }));
 
   SyncedCron.start();
 
