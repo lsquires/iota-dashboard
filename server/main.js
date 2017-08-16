b6d00d0257fe23a17843f515518e9dd782a4691f
Laurence <laurence.j.squires@gmail.com>
2017-08-16 17:33:22 +0100

    Testing new time method

--- server/main.js
+++ server/main.js
@@ -296,7 +296,9 @@ function checkParents(tx) {
 
 function addTX(tx, path) {
   tx.time = new Date().valueOf();
+
   console.log("adding tx: " + tx.time);
+  console.log("alting tx: " + (path.replace(/^.*[\\\/]/, '').split(".")[0]/1000));
   tx.confirmed = false;
   tx.tip = true;
 
