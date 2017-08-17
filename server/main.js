f7414abdf44b32293c5690dccd75faf8f710928e
Laurence <laurence.j.squires@gmail.com>
2017-08-17 12:32:33 +0100

    Added correct initial atm

--- server/main.js
+++ server/main.js
@@ -15,6 +15,16 @@ let currentTime = new ReactiveVar(new Date().valueOf());
 //txs.remove({});
 //stats.remove({date: {$lte: (1502875800000)}});
 
+histographstats.update({set: true}, { $set: {
+  peakTXs: stats.find({},{limit: 1, sort: {TXs: -1}}).fetch().TXs,
+  peakCTXs: stats.find({},{limit: 1, sort: {cTXs: -1}}).fetch().cTXs,
+  peakVol: stats.find({},{limit: 1, sort: {totalTX: -1}}).fetch().totalTX,
+  peakPercent: 0.7947,
+  peakTime: stats.find({},{limit: 1, sort: {averagectimefiltered: 1}}).fetch().averagectimefiltered
+  }
+});
+
+
 Meteor.startup(() => {
 
   function deleteFilesInFolder(path) {
